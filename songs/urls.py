2197029dcf974dbc9adbd3c2f23a56f1612e88e8
손상훈 <son950610@naver.com>
2022-11-03 22:54:27 +0900

    Feat: Comment View,url 생성
    	- Songs  Views.py에서 CommentView 생성
    	- Songs urls.py에서 Comment url 생성

--- songs/urls.py
+++ songs/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:song_id>/', views.SongView.as_view(), name='song_view'),
+    path('<int:song_id>/', views.SongView.as_view(), name='song_view'),    
     path('<int:song_id>/song_like/', views.SongLikeView.as_view(), name='song_like_view'),
+    path('<int:song_id>/comment/', views.CommentView.as_view(), name='comment_view'),
+
 ]
--- songs/views.py
+++ songs/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Song
-from songs.serializers import SongSerializer
+from songs.serializers import SongSerializer,CommentSerializer, CommentCreateSerializer
 # Create your views here.
 
 
@@ -29,3 +29,18 @@ class SearchView(APIView):
     def get(self, request):
         post_result = Song.object.all()
     pass
+
+class CommentView(APIView):
+    def get(self, request, song_id):
+        song = Song.objects.get(id=song_id)
+        comments = song.comment_set.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, song_id):
+        serializer = CommentCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user, article_id=song_id)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
